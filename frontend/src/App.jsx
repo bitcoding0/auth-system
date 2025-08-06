c34bcba5fb27f54cc9a04953de73dec04122db8b
alammd0 <sonualam96314@gmail.com>
2025-08-06 23:43:50 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-500'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
